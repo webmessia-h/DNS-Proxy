5cc53ff2c50bb333bbf644bbd946e8d05c075102
webmessia-h <iamcreepingdeath01@gmail.com>
2024-07-30 20:50:55 +0300

    delete config from root

--- config.c
+++
@@ -1,5 +0,0 @@
-#include "../include/config.h"
-const char *upstreamDNS = "8.8.8.8";
-const char *address = "127.0.0.1";
-const int port = 53;
-const char *BLACKLIST[] = {"microsoft.com", "google.com", "\0"};
